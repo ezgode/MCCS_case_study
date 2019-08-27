ceca66325e0def98aedd97f5155010e666a0b891
Ezequiel <ezgode@gmail.com>
2019-08-27 16:58:40 +0200

    updating the script to automatically compress the files

--- code/functions/super_user_utilities/compressModuleResources.m
+++ code/functions/super_user_utilities/compressModuleResources.m
@@ -1,132 +1,79 @@
-function compressModuleResources
+function compressModuleResourcess
+    %
+    root = [pwd,'/'];
+    %
+    %convertSimulinkFiles
+    %
+    targeted_version = {'2019a','2018a','2018b', '2016a','2016b','2017a','2017b'};
 
-%
-root = '/Users/ezequiel/Dropbox/MatlabCaseStudy-2017-2018/code/';%
-root = [pwd,'/'];%
+    for version_id = 1:length(targeted_version)
+        version_tag = targeted_version{version_id};
 
-%convertSimulinkFiles
-targeted_version = {'2018a', '2016a','2016b','2017a','2017b'};
+        for moduleID = 1:4
+            zipNameSolved = [root, sprintf('data/zipFiles/solved_exercise%01d_MatlabR%s.zip',moduleID,version_tag)];
+            zipNameUnsolved = [root, sprintf('data/zipFiles/exercise%01d_MatlabR%s.zip',moduleID,version_tag)];
 
-for version_id = 1:length(targeted_version)
-    version_tag = targeted_version{version_id};
-    
-    for moduleID = 3
-        
-        zipNameSolved = [root, sprintf('zipFiles/solved_exercise%01d_MatlabR%s.zip',moduleID,version_tag)];
-        zipNameUnsolved = [root, sprintf('zipFiles/exercise%01d_MatlabR%s.zip',moduleID,version_tag)];
-        
-        switch moduleID
-            case 1
-                solvedFiles={
-                    [root,'utilities.m'];
-                    [root,'simulinkFiles/',version_tag,sprintf('/solved_open_loop_experiments_R%s.slx',version_tag)];
-                    [root,'solutions/solEx1.m'];
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    [root,'key.txt'];
-                    [root,'scripts/exercise1_Linearization.m'];};
-                    
-                %
-                unsolvedFiles={
-                    [root,'utilities.m'];
-                    [root,'simulinkFiles/',version_tag,sprintf('/open_loop_experiments_R%s.slx',version_tag)];
-                    [root,'solutions/ex1.m'];
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];                    
-                    [root,'scripts/exercise1_Linearization.m'];};                
-            case 2
-                solvedFiles={
-                    [root,'utilities.m'];
-                    %
-                    [root,'simulinkFiles/',version_tag,sprintf('/solved_LQR_closed_Loop_R%s.slx',version_tag)];
-                    %
-                    [root,'simulinkFiles/',version_tag,sprintf('/solved_LQR_observer_R%s.slx',version_tag)];
-                    %
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    [root,'key.txt'];
-                    %
-                    [root,'solutions/solEx2.m'];
-                    [root,'scripts/exercise2_LQRandObserver.m'];};
-                %
-                unsolvedFiles={
-                    [root,'utilities.m'];
-                    %
-                    [root,'simulinkFiles/',version_tag,sprintf('/LQR_closed_Loop_R%s.slx',version_tag)];
-                    [root,'simulinkFiles/',version_tag,sprintf('/LQR_observer_R%s.slx',version_tag)];
-                    %
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    %
-                    [root,'solutions/ex2.m'];
-                    [root,'scripts/exercise2_LQRandObserver.m'];};
-                            
-            case 3
-                solvedFiles={
-                    [root,'utilities.m'];
-                    [root,'solutions/solEx3.m'];
-                    %
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    [root,'key.txt'];
-                    %
-                    [root,'scripts/exercise3_DynamicProgramming.m'];};
-                %
-                unsolvedFiles={
-                    [root,'utilities.m'];
-                    %
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    %
-                    [root,'solutions/ex3.m'];
-                    [root,'scripts/exercise3_DynamicProgramming.m'];};
-                
-            case 4
-                solvedFiles={
-                    [root,'utilities.m'];
-                    %[root,'simulinkFiles/',version_tag,sprintf('/solved_navigation_function_R%s.slx',version_tag)];
-                    %
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    [root,'key.txt'];
-                    %
-                    [root,'solutions/solEx4.m'];
-                    [root,'scripts/exercise4_NavigationFunction.m'];};
-                %
-                unsolvedFiles={
-                    [root,'utilities.m'];
-                    %[root,'simulinkFiles/',version_tag,sprintf('/navigation_function_R%s.slx',version_tag)];
-                    %
-                    [root,'path_files/circle.mat'];
-                    [root,'path_files/path_1.mat'];
-                    [root,'path_files/path_2.mat'];
-                    [root,'path_files/path_3.mat'];
-                    %
-                    [root,'solutions/ex4.m'];
-                    [root,'scripts/exercise4_NavigationFunction.m'];};                     
+            switch moduleID
+                case 1
+                    common_files = {
+                        [root,'./functions/others/utilities.m'];
+                        [root,'data/inputs/circle.mat'];[root,'data/inputs/path_1.mat'];[root,'data/inputs/path_2.mat'];[root,'data/inputs/path_3.mat'];
+                        [root,'exercises/exercise1_Linearization.m'];};
+
+                    solved_files = {
+                        [root,'simulinkFiles/',version_tag,sprintf('/solved_open_loop_experiments_R%s.slx',version_tag)];
+                        [root,'exercises/solEx1.m'];
+                        [root,'key.txt'];};
+
+                    unsolved_files = {
+                        [root,'./functions/others/utilities.m'];
+                        [root,'simulinkFiles/',version_tag,sprintf('/open_loop_experiments_R%s.slx',version_tag)];};
+
+                case 2
+                    common_files = {
+                        [root,'./functions/others/utilities.m'];
+                        [root,'data/inputs/circle.mat'];[root,'data/inputs/path_1.mat'];[root,'data/inputs/path_2.mat'];[root,'data/inputs/path_3.mat'];
+                        [root,'exercises/exercise2_LQRandObserver.m'];};
+
+                    solved_files = {
+                        [root,'simulinkFiles/',version_tag,sprintf('/solved_LQR_closed_Loop_R%s.slx',version_tag)];
+                        [root,'simulinkFiles/',version_tag,sprintf('/solved_LQR_observer_R%s.slx',version_tag)];
+                        [root,'exercises/solEx2.m'];
+                        [root,'key.txt'];};
+
+                    unsolved_files = {
+                        [root,'simulinkFiles/',version_tag,sprintf('/LQR_closed_Loop_R%s.slx',version_tag)];
+                        [root,'simulinkFiles/',version_tag,sprintf('/LQR_observer_R%s.slx',version_tag)];
+                        [root,'exercises/ex2.m'];};                            
+                case 3
+                    common_files = {
+                        [root,'./functions/others/utilities.m'];
+                        [root,'data/inputs/circle.mat'];[root,'data/inputs/path_1.mat'];[root,'data/inputs/path_2.mat'];[root,'data/inputs/path_3.mat'];
+                        [root,'exercises/exercise3_DynamicProgramming.m'];};                
+
+                    solved_files = {[root,'exercises/solEx3.m'];[root,'key.txt'];};
+                    unsolved_files = {[root,'exercises/ex3.m'];};                         
+
+                case 4
+                    common_files = {
+                        [root,'./functions/others/utilities.m'];
+                        [root,'data/inputs/circle.mat'];[root,'data/inputs/path_1.mat'];[root,'data/inputs/path_2.mat'];[root,'data/inputs/path_3.mat'];
+                        [root,'exercises/exercise4_NavigationFunction.m'];};                
+
+                    solved_files = {[root,'exercises/solEx4.m'];[root,'key.txt'];};
+                    unsolved_files = {[root,'exercises/ex4.m'];};     
+            end
+
+
+            solvedFiles=[common_files;solved_files];
+            unsolvedFiles=[common_files;unsolved_files];
+
+            fprintf('saving %s...\n',zipNameSolved);
+            fprintf('saving %s...\n',zipNameUnsolved);
+
+            zip(zipNameSolved,solvedFiles);
+            zip(zipNameUnsolved,unsolvedFiles);
         end
-        
-        fprintf('saving %s...\n',zipNameSolved);
-        fprintf('saving %s...\n',zipNameUnsolved);
-   
-        zip(zipNameSolved,solvedFiles);
-        zip(zipNameUnsolved,unsolvedFiles);
-
     end
-end
     
 end
